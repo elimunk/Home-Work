4aaa71bf7748da7486385a5ff0479da45beba517
elimunk <43822750+elimunk@users.noreply.github.com>
2018-10-15 01:04:43 +0300

    student

--- 14-10-18/in class/Program.java
+++ 14-10-18/in class/Program.java
@@ -3,9 +3,6 @@ public class Program {
 
 	public static void main(String[] args) {
 
-		int age = 23;
-		int grade = 78;
-
 		Student[] matrix = new Student[9];
 		for (int i = 0; i < matrix.length; i++) {
 			
