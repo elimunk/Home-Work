4830aade93239bba3279f12fe260f5ecb7d79322
elimunk <43822750+elimunk@users.noreply.github.com>
2018-10-20 20:28:25 +0300

    exercise HW

--- 17-10-18/airport/Passenger.java
+++ 17-10-18/airport/Passenger.java
@@ -3,17 +3,17 @@ import java.util.Random;
 public class Passenger extends AirportVisitors {
 	Random ran = new Random();
 
-int Passowrd;	
+int Passport;	
 	
 public Passenger(String firstName, String lastName, String gender, int age, String citizenship) {
 	super(firstName, lastName, gender, age, citizenship);
-	this.Passowrd =ran.nextInt(99999999- 0000001) + 00000001 ;
+	this.Passport =ran.nextInt(99999999- 0000001) + 00000001 ;
 }
 
 @Override
 public String toString() {
 	return   "\n"+ "Passenger:\n" +  super.toString()
-			+" , Passowrd: "+ Passowrd;
+			+" , Passport: "+ Passport;
 }	
 	
 	@Override
