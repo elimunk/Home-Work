74e02c296f819fe0b6aecee52d7eef66c2a5e0e8
elimunk <43822750+elimunk@users.noreply.github.com>
2018-12-18 23:52:05 +0200

    exercise HW

---
+++ 15-12-18/Ex.js
@@ -0,0 +1,49 @@
+var num = 0;
+var sum = 0;
+var avg = 0;
+var biggestNum = 0;
+var smallestNum = 0;
+var bigIndex = 0;
+var smallIndex = 0;
+var sumEvenNun = 0;
+var sumOddNum = 0;
+var sumPozitiveNum = 0;
+var sumNegativeNum = 0;
+//---- define the array----
+var array = new Array(10);
+for (var index = 0; index < array.length; index++) {
+    array[index] = num = Math.floor(Math.random() * 200);
+}
+smallestNum = array[0];
+//---- calculate data---- 
+for (var index = 0; index < array.length; index++) {
+    console.log(array[index]);
+    sum += array[index];
+    if (biggestNum < array[index]) {
+        biggestNum = array[index];
+        bigIndex = index;
+    }
+    if (smallestNum > array[index]) {
+        smallestNum = array[index];
+        smallIndex = index;
+    }
+    if (array[index] % 2 == 0)
+        sumEvenNun += array[index];
+    if (array[index] % 2 == 1)
+        sumOddNum += array[index];
+    if (array[index] > 0)
+        sumPozitiveNum += array[index];
+    if (array[index] == 0)
+        sumNegativeNum += array[index];
+}
+//---- print all data----   
+console.log("sum of all numbers is: " + sum);
+console.log("avrage the numbers in the array is: " + sum / array.length);
+console.log("The biggest number in the array is: " + biggestNum);
+console.log("The smallest  number in the array is: " + smallestNum);
+console.log("The index of the biggest number in the array is: " + bigIndex);
+console.log("The index of the smallest number in the array is: " + smallIndex);
+console.log("sum of the Even numbers is: " + sumEvenNun);
+console.log("sum of the odd numbers is: " + sumOddNum);
+console.log("sum of the Pozitive numbers is: " + sumPozitiveNum);
+console.log("sum of the Negative numbers is: " + sumNegativeNum);
---
+++ 15-12-18/Ex.ts
@@ -0,0 +1,52 @@
+var num: number=0;
+var sum: number=0;
+var avg: number=0;
+var biggestNum: number=0;
+var smallestNum : number=0;
+var bigIndex: number=0;
+var smallIndex: number=0;
+var sumEvenNun: number=0;
+var sumOddNum: number=0;
+var sumPozitiveNum: number=0;
+var sumNegativeNum: number=0;
+                                   //---- define the array----
+var array :number[]= new Array<number>(10);
+for (let index = 0; index < array.length; index++) {
+     array[index]=num=Math.floor(Math.random()*200);
+}
+smallestNum=array[0];
+                                      //---- calculate data---- 
+for (let index = 0; index < array.length; index++) {
+    console.log(array[index]);
+    sum+=array[index];
+    if(biggestNum<array[index]){
+        biggestNum=array[index];
+        bigIndex=index;}
+    if(smallestNum>array[index]){
+        smallestNum=array[index];
+        smallIndex=index;}
+    if(array[index]%2==0)
+    sumEvenNun+=array[index];
+    if(array[index]%2==1)
+    sumOddNum+=array[index];
+    if(array[index]>0)
+    sumPozitiveNum+=array[index];
+    if(array[index]==0)
+    sumNegativeNum+=array[index];
+ }
+                            //---- print all data----   
+ console.log(`sum of all numbers is: ${sum}`);
+ console.log(`avrage the numbers in the array is: ${sum/array.length}`);
+ console.log(`The biggest number in the array is: ${biggestNum}`);
+ console.log(`The smallest  number in the array is: ${smallestNum}`);
+ console.log(`The index of the biggest number in the array is: ${bigIndex}`);
+ console.log(`The index of the smallest number in the array is: ${smallIndex}`);
+ console.log(`sum of the Even numbers is: ${sumEvenNun}`);
+ console.log(`sum of the odd numbers is: ${sumOddNum}`);
+ console.log(`sum of the Pozitive numbers is: ${sumPozitiveNum}`);
+ console.log(`sum of the Negative numbers is: ${sumNegativeNum}`);
+
+
+
+
+
